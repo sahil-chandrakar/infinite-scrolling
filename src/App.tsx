82a28a73131215f52a3064c2dd830c43dc8d802d
sahil-chandrakar <chandrakarsahil2002@gmail.com>
2025-12-01 17:42:45 +0530

    fixed error-handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, ImageList, ImageListItem } from "@mui/material";
+import {
+  Box,
+  ImageList,
+  ImageListItem,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 interface ApiData {
   id: string;
@@ -13,20 +19,29 @@ const App = () => {
   const [images, setImages] = useState<ApiData[]>([]);
   const [page, setPage] = useState<number>(1);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<unknown>();
 
   const getData = async () => {
-    setLoading(true);
+    try {
+      setLoading(true);
+      const res = await axios.get<ApiData[]>(
+        `https://api.unsplash.com/photos?client_id=tyNB_mcZBIXWIoY1GskCrE2tO9KqBW7RUc03lbLoCeM&page=${page}&per_page=20`
+      );
 
-    const res = await axios.get<ApiData[]>(
-      `https://api.unsplash.com/photos?client_id=tyNB_mcZBIXWIoY1GskCrE2tO9KqBW7RUc03lbLoCeM&page=${page}&per_page=20`
-    );
-
-    setImages((prev) => [...prev, ...res.data]);
-    setLoading(false);
+      setImages((prev) => [...prev, ...res.data]);
+    } catch (e) {
+      setError(e);
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    getData();
+    const fetchData = async () => {
+      await getData();
+    };
+    fetchData();
   }, [page]);
 
   useEffect(() => {
@@ -45,21 +60,28 @@ const App = () => {
 
   return (
     <Box>
-      {" "}
-      <ImageList variant="masonry" cols={6} gap={10} sx={{ padding: "10px" }}>
-        {" "}
+      <Typography
+        variant="h3"
+        sx={{
+          textAlign: "center",
+          margin: "15px 0",
+          fontWeight: "700",
+        }}
+      >
+        Infinite Scrolling
+      </Typography>
+      <ImageList cols={6} gap={10} sx={{ padding: "10px" }}>
         {images.map((val, index) => (
           <ImageListItem key={index} sx={{ overflow: "hidden" }}>
-            {" "}
             <img
               src={val.urls.small}
               loading="lazy"
               style={{ borderRadius: 10, display: "block" }}
-            />{" "}
+            />
           </ImageListItem>
-        ))}{" "}
-      </ImageList>{" "}
-      {loading && <p style={{ textAlign: "center" }}>Loading...</p>}{" "}
+        ))}
+      </ImageList>
+      {loading && <p style={{ textAlign: "center" }}>Loading...</p>}
     </Box>
   );
 };
